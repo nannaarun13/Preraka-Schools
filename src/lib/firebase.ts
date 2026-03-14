b7193a6ea79e339515a733e25cc85c82e7995c75
nannaarun <arunnanna3@gmail.com>
2026-03-15 00:00:01 +0530

    Refactor Firebase setup and configuration
    Updated Firebase configuration and removed unused imports.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,39 +1,20 @@
-// Firebase SDK imports
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnLM2N6dn2RcemYmAhcg4J-1Z6dOYIFCc",
-  authDomain: "preraka-schools.firebaseapp.com",
-  databaseURL: "https://preraka-schools-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "preraka-schools",
-  storageBucket: "preraka-schools.appspot.com",
-  messagingSenderId: "311466137615",
-  appId: "1:311466137615:web:0014abda8334496f97ab7a",
-  measurementId: "G-KT86JCXQN4"
+  apiKey: "AIzaSyDteb7H98-XxmI0rmcFfPR6lqAiwdhy7SU",
+  authDomain: "preraka-school.firebaseapp.com",
+  projectId: "preraka-school",
+  storageBucket: "preraka-school.firebasestorage.app",
+  messagingSenderId: "886385961667",
+  appId: "1:886385961667:web:a9da80e2af5a7d5e4fd076",
+  measurementId: "G-4NB208KDS1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
-// Firebase services
-export const auth = getAuth(app);
+// Initialize Cloud Firestore and export it as 'db'
 export const db = getFirestore(app);
-export const storage = getStorage(app);
-
-// Fix for React / Next.js analytics
-let analytics:any = null;
-
-isSupported().then((yes) => {
-  if (yes) {
-    analytics = getAnalytics(app);
-  }
-});
-
-export { analytics };
-
-export default app;
