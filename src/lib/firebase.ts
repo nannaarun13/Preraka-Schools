076d100d3ad889c2cc773a3db58f03749f3b9b61
nannaarun <arunnanna3@gmail.com>
2026-03-15 21:56:50 +0530

    Refactor Firebase setup by removing Firestore
    Removed Firestore initialization and export.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDteb7H98-XxmI0rmcFfPR6lqAiwdhy7SU",
   authDomain: "preraka-school.firebaseapp.com",
@@ -14,7 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-
-// Initialize Cloud Firestore and export it as 'db'
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
