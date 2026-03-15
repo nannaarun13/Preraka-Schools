f120b9084d72704565a07d95065f7de9b16f3b08
nannaarun <arunnanna3@gmail.com>
2026-03-15 23:48:36 +0530

    Create uploadImage.ts

---
+++ src/utils/uploadImage.ts
@@ -0,0 +1,21 @@
+```ts
+export const uploadImage = async (file: File) => {
+
+  const formData = new FormData()
+
+  formData.append("file", file)
+  formData.append("upload_preset", "school_upload")
+
+  const response = await fetch(
+    "https://api.cloudinary.com/v1_1/dpie6aqzf/image/upload",
+    {
+      method: "POST",
+      body: formData
+    }
+  )
+
+  const data = await response.json()
+
+  return data.secure_url
+}
+```
