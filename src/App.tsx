1b0db1664ace78bce74a4cb7c357df15be6fb30e
nannaarun <arunnanna3@gmail.com>
2026-03-19 12:26:50 +0000

    FIX Layout + Header + Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// 1. Change BrowserRouter to HashRouter
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 import { SchoolProvider } from "./contexts/SchoolContext";
 import RouteProtection from "./components/RouteProtection";
 import Layout from "./components/Layout";
+
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Admissions from "./pages/Admissions";
@@ -27,8 +27,7 @@ const App = () => (
       <SchoolProvider>
         <Toaster />
         <Sonner />
-        
-        {/* 2. Replace <BrowserRouter> with <HashRouter> */}
+
         <HashRouter>
           <RouteProtection>
             <Routes>
@@ -43,6 +42,7 @@ const App = () => (
                 <Route path="admin/register" element={<AdminRegistration />} />
                 <Route path="admin/*" element={<AdminDashboard />} />
               </Route>
+
               <Route path="*" element={<NotFound />} />
             </Routes>
           </RouteProtection>
